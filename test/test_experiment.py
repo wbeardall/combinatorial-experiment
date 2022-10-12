778ae5e54b67c6a3aa256f75b38abbe0cc78a722
Will <wb815@ic.ac.uk>
2022-10-12 22:02:18 +0100

    Moved tests -> test

---
+++ test/test_experiment.py
@@ -0,0 +1,19 @@
+import json
+import os
+from pathlib import Path
+import tempfile
+
+from combinatorial_experiment import CombinatorialExperiment, variables
+
+def experiment_function(config):
+    return {'called':json.dumps(config),'run_ok':True}
+
+def test_run_experiment():
+    argv = ['--base_config',os.path.join(Path(__file__).parent.absolute(),'configs','base_config.yml'),
+            '--experiment_config',os.path.join(Path(__file__).parent.absolute(),'configs','experiment_config.yml'),
+            '--output',os.path.join(tempfile.gettempdir(),'_comb_exp_output'),]
+    records = CombinatorialExperiment.run_experiment(experiment_function,argv=argv)
+    var = variables.deserialize_experiment_config(os.path.join(Path(__file__).parent.absolute(),'configs','experiment_config.yml'))
+    assert len(records)==len(var)
+    for el in records['run_ok']:
+        assert el

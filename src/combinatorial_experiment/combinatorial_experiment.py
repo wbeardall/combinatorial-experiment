4aecedafa7a4031503091ac791b004855f6e9668
Will <wb815@ic.ac.uk>
2022-11-24 13:52:28 +0000

    Added flattened dir structure, relpath and field prefixes.

--- src/combinatorial_experiment/combinatorial_experiment.py
+++ src/combinatorial_experiment/combinatorial_experiment.py
@@ -13,9 +13,11 @@ import multiprocess as mp
 import numpy as np
 import os
 import pandas as pd
+import random
 import re
 import time
 from tqdm import tqdm
+import string
 import sys
 import warnings
 import yaml
@@ -40,26 +42,26 @@ def multiprocess_wrap(func):
 # failed runs, etc.
 class CombinatorialExperiment(object):
     """Combinatorial Test object.
-    
-    This class runs arbitrary tests defined in experiment_function, with the 
+
+    This class runs arbitrary tests defined in experiment_function, with the
     variables provided. This class supports caching through Dill, which means
     that it is not secure. Never run a .tst object from an untrustworthy source,
     as the experiment_function can contain arbitrary (and possibly malicious) code.
-    
+
     """
-    
     _cache_base="test_iter"
     _cache_ext = ".tst"
-    
-    
-    def __init__(self, 
-                 experiment_function=None, 
-                 variables=None, 
-                 experiment_dir=os.getcwd(), 
+
+
+    def __init__(self,
+                 experiment_function=None,
+                 variables=None,
+                 experiment_dir=os.getcwd(),
                  resume=True,
                  base_config={},
                  additional_config={},
                  autoname=False,
+                 heirarchical_dirs=False,
                  repeats=1):
         #NOTE: cache is deprecated.
         if experiment_function is None or variables is None:
@@ -87,13 +89,14 @@ class CombinatorialExperiment(object):
         base_config.update(additional_config)
         self.base_config=NestedDict(base_config)
         self.autoname=autoname
+        self.heirarchical_dirs=heirarchical_dirs
         self.repeats=repeats
         self.set_directories(experiment_dir,resume)
         try:
             mp.set_start_method('spawn')
         except RuntimeError:
             pass
-        
+
     @classmethod
     def run_experiment(cls,experiment_function,add_cmd_args=[],argv=None):
         parser = cls.parser()
@@ -104,7 +107,7 @@ class CombinatorialExperiment(object):
         if argv is None:
             argv = sys.argv[1:]
         args = parser.parse_args(argv)
-        
+
         d = vars(args)
         experiment = cls.from_config(
                         experiment_function,
@@ -113,10 +116,11 @@ class CombinatorialExperiment(object):
                         base_config=args.base_config,
                         additional_config = {el:d[el] for el in add_cmd_args},
                         autoname=args.autoname,
+                        heirarchical_dirs=args.heirarchical_dirs,
                         repeats = args.repeats
             )
         return experiment.run()
-        
+
     @classmethod
     def from_config(cls,
                     experiment_function,
@@ -125,6 +129,7 @@ class CombinatorialExperiment(object):
                     base_config={},
                     additional_config={},
                     autoname=False,
+                    heirarchical_dirs=False,
                     repeats=1
                     ):
         variables = deserialize_experiment_config(experiment_config)
@@ -134,9 +139,10 @@ class CombinatorialExperiment(object):
                    base_config=base_config,
                    additional_config=additional_config,
                    autoname=autoname,
+                   heirarchical_dirs=heirarchical_dirs,
                    repeats=repeats
                    )
-    
+
     @classmethod
     def parser(cls):
         parser = argparse.ArgumentParser()
@@ -145,8 +151,9 @@ class CombinatorialExperiment(object):
         parser.add_argument('--output',default=None)
         parser.add_argument('--repeats',type=int,default=1)
         parser.add_argument('--autoname',action="store_true")
+        parser.add_argument('--heirarchical_dirs',action="store_true")
         return parser
-    
+
     @classmethod
     def resume(cls,path):
         assert os.path.exists(os.path.join(path,'cache'))
@@ -154,32 +161,32 @@ class CombinatorialExperiment(object):
                                              experiment_dir=path,
                                              resume=True)
         experiment.run()
-        
+
     def add_variable(self, variable):
         self._variables.update(variable)
-        
+
     def check_run_complete(self,directory=None):
         if directory is None:
             directory = self._experiment_dir
         return os.path.exists(os.path.join(directory,'.run_complete'))
-        
+
     def mark_run_complete(self):
         with open(os.path.join(self._experiment_dir,'.run_complete'),'w') as f:
             f.write(str(datetime.datetime.now()))
-            
+
     def make_gitignore(self):
         gitignore = ["cache",
                      "experiments",
                      "input"]
         with open(os.path.join(self._experiment_dir,'.gitignore'),'w') as f:
                   f.writelines(gitignore)
-                  
+
     def dump_configs(self):
         with open(os.path.join(self._experiment_dir,'base_config.yml'),'w') as f:
             yaml.dump(dict(self.base_config),f)
         with open(os.path.join(self._experiment_dir,'experiment_config.yml'),'w') as f:
             yaml.dump(self._variables.serialize(),f)
-            
+
     def set_directories(self,experiment_dir,resume):
         experiment_dir = os.path.abspath(experiment_dir)
         tail,head = os.path.split(experiment_dir)
@@ -209,7 +216,7 @@ class CombinatorialExperiment(object):
             experiment_dir = safe_save(experiment_dir,zero_ext=False,ignore_ext=True)
             self._experiment_dir = experiment_dir
             print("\n\nStarting new experiment in {}\n\n".format(experiment_dir))
-            
+
     def setup(self):
         if not os.path.exists(self._cache_dir):
             os.makedirs(self._cache_dir)
@@ -226,14 +233,14 @@ class CombinatorialExperiment(object):
             if isinstance(hdlr,logging.FileHandler):
                 self.logger.removeHandler(hdlr)
         self.logger.addHandler(filehandler) # set the new handler
-        
-            
+
+
     @property
     def _cache_dir(self):
         cache_dir = os.path.join(self._experiment_dir,'cache')
         return cache_dir
-    
-        
+
+
     def run(self):
         self.setup()
         self.dump_configs()
@@ -262,7 +269,7 @@ class CombinatorialExperiment(object):
         self.save_results(False)
         self.mark_run_complete()
         return self.records
-    
+
     def run_iteration(self,parameters,i,run_length):
 
         if isinstance(parameters, Parameter):
@@ -281,46 +288,62 @@ class CombinatorialExperiment(object):
                 records.update(p.record)
                 if p.name in variables_len_gt_one:
                     dir_dict[p.name]=str(p)
-            if self.repeats>1:
-                # Repeats always at head of directory tree
-                savedir = os.path.join(savedir,'repeat--{}'.format(j))
-            # K-Fold always at tail of directory tree, External, then Internal
-            ef_key = None
-            if_key = None
-            for k in dir_dict.keys():
-                if re.match('ext[0-9]+fold',k):
-                    ef_key = k
-                if re.match('int[0-9]+fold',k):
-                    if_key = k
-            if ef_key:
-                efold_dir = dir_dict.pop(ef_key)
+
+
+            # Flatten records to dict and prepend CFG to all
+            records = {f"CFG_{k}":v for k,v in NestedDict.flatten(records).items()}
+
+            if self.heirarchical_dirs:
+                if self.repeats>1:
+                    # Repeats always at head of directory tree
+                    savedir = os.path.join(savedir,'repeat--{}'.format(j))
+                # K-Fold always at tail of directory tree, External, then Internal
+                ef_key = None
+                if_key = None
+                for k in dir_dict.keys():
+                    if re.match('ext[0-9]+fold',k):
+                        ef_key = k
+                    if re.match('int[0-9]+fold',k):
+                        if_key = k
+                if ef_key:
+                    efold_dir = dir_dict.pop(ef_key)
+                else:
+                    efold_dir = None
+                if if_key:
+                    ifold_dir = dir_dict.pop(if_key)
+                else:
+                    ifold_dir = None
+                savedir = os.path.join(savedir,'/'.join([dir_dict[k] for k in sorted(dir_dict)]))
+                for el in [efold_dir,ifold_dir]:
+                    if el:
+                        savedir = os.path.join(savedir,el)
             else:
-                efold_dir = None
-            if if_key:
-                ifold_dir = dir_dict.pop(if_key)
-            else:
-                ifold_dir = None
-            savedir = os.path.join(savedir,'/'.join([dir_dict[k] for k in sorted(dir_dict)]))
-            for el in [efold_dir,ifold_dir]:
-                if el:
-                    savedir = os.path.join(savedir,el)
+                while True:
+                    cand = os.path.join(savedir,
+                        "".join(
+                            random.choice(string.ascii_lowercase) for i in range(15)
+                        ))
+                    if not os.path.exists(cand):
+                        savedir=cand
+                        break
+
             try:
-                
-                try:
-                    os.makedirs(savedir)
-                except FileExistsError:
-                    # Resuming from this directory
-                    pass
-                
-                # Save config
-                with open(os.path.join(savedir,'experiment_config.yml'),'w') as f:
-                    yaml.dump(config,f)
-                
-                
-                config.update({'savedir':savedir})
-                    
+                os.makedirs(savedir)
+            except FileExistsError:
+                # Resuming from this directory
+                pass
+
+            # Save config
+            with open(os.path.join(savedir,'experiment_config.yml'),'w') as f:
+                yaml.dump(config,f)
+
+
+            records['CFG_relpath'] = os.path.relpath(savedir,self._experiment_dir)
+            config.update({'savedir':savedir})
+
+            try:
                 q = mp.Queue()
-                    
+
                 p = mp.Process(target=self.experiment_function,
                                args = (q,config))
                 # TODO: Consider making this more flexible, i.e. constructing
@@ -335,47 +358,45 @@ class CombinatorialExperiment(object):
                 gc.collect()
                 if 'metrics' in output.keys():
                     metrics = output['metrics']
-                    
+
                 else:
                     metrics = output
-                
-                # Automatically record walltime if not in metrics
-                if not any(t_symbol in metrics.keys() for t_symbol in allowed_time_symbols):
-                    metrics['t'] = outer_walltime
-                
-                
+
+
             except Exception as e:
                 self.logger.exception("Fatal error in experiment #{} ({}):\n{}".format(i,savedir,e))
                 warnings.warn(str(e))
                 # Ensure we aren't saving metrics from a partial run
                 metrics = {}
-                
-            records.update(metrics)
-            
+
+            records.update({k if k.beginswith("METRIC_") or k.beginswith("ENG_")
+                else f"METRIC_{k}":v for k,v in metrics.items()})
+            records.update({"ENG_walltime":outer_walltime})
+
             if self._records is None:
-                self._records = pd.DataFrame(data=NestedDict.flatten(records), index=[0])
+                self._records = pd.DataFrame(data=records, index=[0])
             else:
-                self._records = self._records.append(NestedDict.flatten(records), ignore_index=True)
+                self._records = self._records.append(records, ignore_index=True)
             self.save_results(True)
 
-    
+
     def save_results(self,intermediate=False):
         if intermediate:
             prepend = 'intermediate_'
         else:
             prepend = ''
         self._records.to_csv(os.path.join(self._experiment_dir,'{}results.csv'.format(prepend)))
-        
+
         with open(os.path.join(self._experiment_dir,'{}results.md'.format(prepend)), 'w') as f:
             if use_style:
                 f.write(self._records.style.to_latex())
             else:
                 f.write(self._records.to_latex(index=False,float_format='%0.3f'))
-    
+
     @property
     def records(self):
        return self._records
-   
+
     def auto_deserialize(self,directory):
         filename = os.path.join(directory,self._cache_base+self._cache_ext)
         try:
@@ -391,11 +412,11 @@ class CombinatorialExperiment(object):
                     #os.remove(file)
         except FileNotFoundError:
             print("Cache file not found.")
-    
+
     def serialize(self, filename):
         with open(filename,'wb') as f:
             pickle.dump(self,f)
-       
+
     def deserialize(self,filename):
         with open(filename, 'rb') as f:
             self.__dict__.update(pickle.load(f).__dict__)
